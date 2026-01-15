35a7716e87309f640c7768e74e61a424dfb17c5a
Chloe <chloe@Chloe.C00517862@louisiana.edu>
2026-01-14 21:10:33 -0600

    Added Tag Routes

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import items, categories, auth
+from app.api.routes import items, categories, auth, tags
 
 api_router = APIRouter()
 api_router.include_router(items.router)
 api_router.include_router(categories.router)
 api_router.include_router(auth.router)
+api_router.include_router(tags.router)
